4266d722c274c1b2713c42f57bde7ff7f4add708
Jesse A. Tov <jesse.tov@gmail.com>
2020-06-27 17:51:14 -0500

    (cargo-release) version 0.2.4

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#![doc(html_root_url = "https://docs.rs/weak-table/0.3.0")]
+#![doc(html_root_url = "https://docs.rs/weak-table/0.2.4")]
 //! This library offers a variety of weak hash tables:
 //!
 //!   - For a hash map where the keys are held by weak pointers and compared by key value, see
