180acc8b8f8d35421dace966dfe7781c9c36ffc3
Nick Mathewson <nickm@torproject.org>
2026-03-24 11:41:18 -0400

    Begin documenting internals.

--- src/inner.rs
+++ src/inner.rs
@@ -1,8 +1,34 @@
+//! Internal implementation for weak tables, based on [`hashbrown`].
+//!
+//! Rather than write our own hashtable implementation,
+//! we're using `hashbrown` as a backend,
+//! since it uses a lot of tricky techniques (SIMD! Swisstables!)
+//! to keep performance high.
+//! (It's also the backend used by Rust's standard library.)
+//!
+//! To avoid duplicated code,
+//! we define a single [`Table`] type to serve as the backend for all of our weak tables,
+//! and a pair of wrappers for [`Weak`] and [`Owned`] keys and values to hide
+//! the difference between the two types.
+//! These wrapper types implement internal [`Element`] and [`Key`] traits
+//! that the table code uses.
+//!
+//! For example, a [`WeakKeyHashMap`] is internally implemented as a
+//! `Table<Weak<K>, Owned<V>>`.
+//!
+//! [`WeakKeyHashMap`]: super::WeakKeyHashMap
 mod table;
 mod wrappers;
 
 pub(crate) use table::*;
 pub(crate) use wrappers::*;
 
+/// Alias for a weak wrapper for a key.
+///
+/// Since this is a key, we need to store its hash.
 pub(crate) type WeakK<T> = Weak<T, u64>;
+
+/// Alias for a weak wrapper for a value.
+///
+/// Since this is a value, we do not need to store its hash.
 pub(crate) type WeakV<T> = Weak<T, ()>;
--- src/inner/wrappers.rs
+++ src/inner/wrappers.rs
@@ -1,3 +1,7 @@
+//! Wrappers around Weak and Owned types, used to implement our internal [`Table`].
+//!
+//! [`Table`]: super::Table
+
 use crate::compat::*;
 
 use crate::{
@@ -5,50 +9,175 @@ use crate::{
     util::hash_one,
 };
 
+/// A type that can be Owned or Weak.
+///
+/// Since this needs to be implemented by our [`Owned`] wrapper,
+/// we cannot use [`WeakElement`] for this.
 pub(crate) trait Element: Sized {
+    /// The owned type corresponding to this type.
+    ///
+    /// | Element type | Self               | Owned                  |
+    /// | ------------ | ------------------ | ---------------------- |
+    /// | Weak         | `Weak<WeakPtr<T>>` | `StrongPtr<T>`         |
+    /// | Owned        | `Owned<T>`         | `T`                    |
     type Owned;
+
+    /// A reference to this type, that our APIs return.
+    ///
+    /// | Element type | Self               |  Ref                   |
+    /// | ------------ | ------------------ | ---------------------- |
+    /// | Weak         | `Weak<WeakPtr<T>>` | `StrongPtr<T>`         |
+    /// | Owned        | `Owned<T>`         | `&T`                   |
     type Ref<'a>
     where
         Self: 'a;
+
+    /// A value that we must hold in order to prevent this element from "disappearing".
+    ///
+    /// We hold this in our [`Entry`](super::Entry) types
+    /// so that weak pointers do not expire while an `Entry` is live.
+    ///
+    /// | Element type | Self               |  Upgraded              |
+    /// | ------------ | ------------------ | ---------------------- |
+    /// | Weak         | `Weak<WeakPtr<T>>` | `StrongPtr<T>`         |
+    /// | Owned        | `Owned<T>`         | `()`                   |
     type Upgraded;
+
+    /// A value that we need to store in order to be able to recover the hash of
+    /// this element.
+    ///
+    /// We need to store hashes for weak elements because
+    /// if they disappear, we can no longer compute their hashes,
+    /// but `hashbrown` requires that keys never change their hashes.
+    ///
+    /// | Element type | Self                    |  CachedHash       |
+    /// | ------------ | ----------------------- | ----------------- |
+    /// | Weak key     | `Weak<WeakPtr<T>, u64>` | `u64`             |
+    /// | Weak value   | `Weak<WeakPtr<T>, ()>`  | `()`              |
+    /// | Owned        | `Owned<T>`              | `()`              |
     type CachedHash: MaybeHash;
 
+    /// Try to return a [`Ref`](Self::Ref) for this value.
+    ///
+    /// Return None if this is an expired weak pointer.
+    ///
+    /// | Element type | Input              | Output                             |
+    /// | ------------ | ------------------ | ---------------------------------- |
+    /// | Weak         | `&Weak<WeakPtr<T>>`| `Option<StrongPtr<T>>`             |
+    /// | Owned        | `&Owned<T>`,       | `Some(&T)`                         |
     fn as_ref<'a>(&'a self) -> Option<Self::Ref<'a>>;
 
+    /// Return true if this is an expired weak pointer.
     fn is_expired(&self) -> bool;
 
+    /// Return any value that we need to hold in order to keep this element from
+    /// disappearing.
+    ///
+    /// | Element type | Input               | Output                 |
+    /// | ------------ | ------------------- | ---------------------- |
+    /// | Weak         | `&Weak<WeakPtr<T>>` | `Option<StrongPtr<T>>` |
+    /// | Owned        | `&Owned<T>`         | `Some(())`             |
     fn upgrade(&self) -> Option<Self::Upgraded>;
 
+    /// Create a new instance of this element from its Owned form and a hash value.
+    ///
+    /// Return this element, and a `Upgraded` handle to hold
+    /// in order to prevent this this element from disappearing.
+    ///
+    /// | Element type | Input             | Output                             |
+    /// | ------------ | ----------------- | ---------------------------------- |
+    /// | Weak         | `Strong<T>`, `u64`| `(Weak<WeakPtr<T>>, StrongPtr<T>)` |
+    /// | Owned        | `T`, `u64`        | `(T, ())`                          |
     fn from_owned(owned: Self::Owned, cached_hash: Self::CachedHash) -> (Self, Self::Upgraded);
 
+    /// Try to convert this element into its owned form.
+    ///
+    /// | Element type | Input              | Output                             |
+    /// | ------------ | ------------------ | ---------------------------------- |
+    /// | Weak         | `Weak<WeakPtr<T>>` | `Option<StrongPtr<T>>`             |
+    /// | Owned        | `Owned<T>`,        | `Some(T))`                         |
     fn into_owned(self) -> Option<Self::Owned>;
 
+    /// Convert a reference this element,
+    /// plus a reference to an `Upgraded` handle,
+    /// into a reference to the Owned form of this element.
+    ///
+    /// | Element type | Input                                | Output          |
+    /// | ------------ | ------------------------------------ | --------------- |
+    /// | Weak         | `&Weak<WeakPtr<T>>`, `&StrongPtr<T>` | `&StrongPtr<T>` |
+    /// | Owned        | `&Owned<T>`, `&()`                   | `&T`            |
     fn owned_ref_from_upgrade<'a>(&'a self, upgraded: &'a Self::Upgraded) -> &'a Self::Owned;
 
+    /// Convert an owned instance of this element,
+    /// plus an owned instance of an `Upgraded` handle,
+    /// into an Owned instance of this element.
+    ///
+    /// | Element type | Input                              | Output         |
+    /// | ------------ | ---------------------------------- | -------------- |
+    /// | Weak         | `Weak<WeakPtr<T>>`, `StrongPtr<T>` | `StrongPtr<T>` |
+    /// | Owned        | `Owned<T>`, `()`                   | `T`            |
     fn owned_from_upgrade(self, upgraded: Self::Upgraded) -> Self::Owned;
 
+    /// Given a reference to an owned copy of the element,
+    /// return an `Upgraded` handle that we have to hold
+    /// to keep the owned copy from disappearing if it is downgraded
+    /// to a weak pointer.
+    ///
+    /// | Element type | Input           | Output         |
+    /// | ------------ | --------------- | -------------- |
+    /// | Weak         | `&StrongPtr<T>` | `StrongPtr<T>` |
+    /// | Owned        | `&T`            | `()`           |
     fn upgraded_from_owned(owned: &Self::Owned) -> Self::Upgraded;
 
-    fn reset_from_upgrade(&mut self, _upgraded: &Self::Upgraded);
+    /// Change this element to correspond to the provided Upgraded handle,
+    /// assuming that they are equal.
+    ///
+    /// | Element type | Input                                    | Behavior |
+    /// | ------------ | ---------------------------------------- | -------- |
+    /// | Weak         | `&mut Weak<WeakPtr<T>>`, `&StrongPtr<T>` | `self = upgraded.downgrade()` |
+    /// | Owned        | `&mut Owned<T>`, `&T`                    | {}       |
+    fn reset_from_upgrade(&mut self, upgraded: &Self::Upgraded);
 }
 
+/// A type that can be Owned or Weak, and which can be used as key.
+///
+/// Since this needs to be implemented by our [`Owned`] wrapper,
+/// we cannot use [`WeakKey`] for this.
 pub(crate) trait Key: Element + Sized {
+    /// The underlying key type that
     type Key: ?Sized;
 
+    /// Compute a hash of this element using a provided hash-builder.
+    ///
+    /// (For weak elements, the hash needs to be pre-computed and stored.)
     fn hash<S: BuildHasher>(&self, hash_builder: &S) -> u64;
 
+    /// Compute a hash of an owned instance of this element.
     fn hash_owned<S: BuildHasher>(strong: &Self::Owned, hash_builder: &S) -> u64;
 
+    /// Return true if this element is not expired, and it is equal to a given
+    /// owned value.
+    ///
+    /// Always return false for expired Weak pointers.
     fn eq_owned(&self, strong: &Self::Owned) -> bool;
 
+    /// Return true if this element is semantically equivalent to key that it
+    /// can be borrowed as.
+    ///
+    /// Always return false for expired Weak pointers.
     fn eq_borrow<Q>(&self, key: &Q) -> bool
     where
         Q: ?Sized + Hash + Eq,
         Self::Key: Borrow<Q>;
 }
 
+/// An owned element in a [`Table`](super::Table).
+///
+/// This is used internally to hold the keys in a [`WeakValueHashMap`](crate::WeakValueHashMap),
+/// or the values in a [`WeakKeyHashMap`](crate::WeakKeyHashMap).
 #[derive(Clone, Debug)]
 pub(crate) struct Owned<T> {
+    /// The actual owned value.
     pub(crate) val: T,
 }
 
@@ -116,6 +245,10 @@ impl<T: Hash + Eq> Key for Owned<T> {
     }
 }
 
+/// A value that might need to store a persistent hash.
+///
+/// This is a `u64` for weak keys,
+/// and a `()` everywhere else.
 pub(crate) trait MaybeHash {
     fn new(hash: u64) -> Self;
 }
@@ -132,9 +265,20 @@ impl MaybeHash for u64 {
     }
 }
 
+/// A weak element in a [`Table`](super::Table).
+///
+/// This is used internally to hold the values in a [`WeakValueKeyMap`](crate::WeakValueHashMap),
+/// the keys in a [`WeakKeyHashMap`](crate::WeakKeyHashMap),
+/// and _all_ elements in a [`WeakWeakHashMap`](crate::WeakWeakHashMap).
 #[derive(Clone, Debug)]
 pub(crate) struct Weak<T, H> {
+    /// An instance of some [`WeakElement`].
     pub(crate) val: T,
+    /// The computed hash of `val`, if this is a key.
+    ///
+    /// (We need to store this for weak keys,
+    /// since if the `WeakElement` disappears,
+    // we can no longer compute the same hash.)
     pub(crate) hash: H,
 }
 
